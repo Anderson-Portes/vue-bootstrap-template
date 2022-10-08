c16382db5fc756d3af11bda60c8062b16513ee83
Anderson Portes <portsanderson@gmail.com>
2022-10-08 15:41:36 -0300

    Adding Poppins font-family

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,48 +1,57 @@
 <template>
-<nav class="navbar navbar-expand-lg border-bottom shadow-sm">
-  <div class="container">
-    <a class="navbar-brand" href="#">Project</a>
-    <button 
-      class="navbar-toggler" 
-      type="button" 
-      data-bs-toggle="collapse" 
-      data-bs-target="#header-navbar" 
-      aria-controls="header-navbar" 
-      aria-expanded="false" 
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end" id="header-navbar">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link :to="{ name: 'Home' }" class="nav-link">
-            Home
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'Register' }" class="nav-link">
-            Register
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'Login' }" class="nav-link">
-            Login
-          </router-link>
-        </li>
-        <!-- <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li> -->
-      </ul>
+  <nav class="navbar navbar-expand-lg border-bottom shadow-sm">
+    <div class="container">
+      <a class="navbar-brand" href="#">Project</a>
+      <button 
+        class="navbar-toggler" 
+        type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#header-navbar" 
+        aria-controls="header-navbar" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="header-navbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link :to="{ name: 'Home' }" class="nav-link">
+              Home
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'Register' }" class="nav-link">
+              Register
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'Login' }" class="nav-link">
+              Login
+            </router-link>
+          </li>
+          <!-- <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li> -->
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
+
+<style scoped>
+  .nav-link {
+    color: black;
+  }
+  .nav-link:hover {
+    color: dimgray;
+  }
+</style>
