39a9c4f8f44ba83bf47eebd0a5f938a86175111d
Anderson Portes <portsanderson@gmail.com>
2022-10-08 13:44:06 -0300

    Adding the state management base files

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  
+  state: {},
+
+  actions: {},
+
+  mutations: {},
+
+  getters: {},
+
+  modules: {}
+})
+
+export default store
